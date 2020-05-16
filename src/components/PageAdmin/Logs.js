c342f864b3b20dbb83376be70d0c2947ef0e19dc
philippemnoel <philippe_noel@college.harvard.edu>
2020-05-16 16:45:40 -0400

    fixed formatting with versioning

--- src/components/PageAdmin/Logs.js
+++ src/components/PageAdmin/Logs.js
@@ -177,15 +177,15 @@ class Logs extends Component {
               </div>
               :
               <div style = {{maxHeight: 600, overflowY: 'scroll', marginTop: 25}}>
-                <table style = {{width: 1000}}>
+                <table style = {{width: 900}}>
                   {this.props.logs.slice(0, Math.min(this.props.logs.length, this.state.last_index)).map((value, index) => {
                     return (
-                      <tr style = {{fontSize: 15, height: 50, padding: 10, paddingBottom: 20, key: "logs"}}>
-                        <td style = {{width: 125}}><a target = "_blank" rel="noopener noreferrer" href = {value["server_logs"]} style = {{background: 'rgba(94, 195, 235, 0.1)', padding: '10px 12px', borderRadius: 2, fontWeight: 'bold'}}><span style = {{color: '#1ba8e0'}}>Server Logs</span></a></td>
-                        <td style = {{width: 125}}><a target = "_blank" rel="noopener noreferrer" href = {value["client_logs"]} style = {{background: 'rgba(2, 207, 57, 0.1)', padding: '10px 12px', borderRadius: 2, fontWeight: 'bold'}}><span style = {{color: '#02cf39'}}>Client Logs</span></a></td>
+                      <tr style = {{fontSize: 12, height: 50, padding: 10, paddingBottom: 20, key: "logs"}}>
+                        <td style = {{width: 100}}><a target = "_blank" rel="noopener noreferrer" href = {value["server_logs"]} style = {{background: 'rgba(94, 195, 235, 0.1)', padding: '10px 12px', borderRadius: 2, fontWeight: 'bold'}}><span style = {{color: '#1ba8e0'}}>Server Logs</span></a></td>
+                        <td style = {{width: 100}}><a target = "_blank" rel="noopener noreferrer" href = {value["client_logs"]} style = {{background: 'rgba(2, 207, 57, 0.1)', padding: '10px 12px', borderRadius: 2, fontWeight: 'bold'}}><span style = {{color: '#02cf39'}}>Client Logs</span></a></td>
                         <td style = {{textAlign: 'center', maxWidth: 100}}>{value["last_updated"]}</td>
                         <td>Connection {value["connection_id"]}</td>
-                        <td style = {{maxWidth: 100}}>Version {value["version"]}</td>
+                        <td style = {{maxWidth: 200}}>Version {value["version"]}</td>
                         {
                         value["username"]
                         ?
@@ -193,7 +193,7 @@ class Logs extends Component {
                         :
                         <td style = {{color: "#888888"}}>No username</td>
                         }
-                        <Button onClick = {() => this.deleteLogs(value["connection_id"])} style = {{marginLeft: 120, background: 'rgba(232, 78, 78, 0.1)', bottom: 0, padding: '4px 8px', borderRadius: 2, border: 'none', fontWeight: 'bold', color: '#e30b0b'}}>X</Button>               
+                        <Button onClick = {() => this.deleteLogs(value["connection_id"])} style = {{marginLeft: 40, background: 'rgba(232, 78, 78, 0.1)', bottom: 0, padding: '4px 8px', borderRadius: 2, border: 'none', fontWeight: 'bold', color: '#e30b0b'}}>X</Button>               
                       </tr>
                     )
                   })}
