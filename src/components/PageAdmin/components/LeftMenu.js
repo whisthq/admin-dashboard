9a1330865bc741bb2a1d31331233e42762824b4b
Jonathan <jonathanxu25@gmail.com>
2020-06-08 15:30:34 -0400

    recharts with regions

--- src/components/PageAdmin/components/LeftMenu.js
+++ src/components/PageAdmin/components/LeftMenu.js
@@ -27,104 +27,110 @@ class LeftMenu extends Component {
         this.setState({ width: window.innerWidth, height: window.innerHeight })
     }
 
-    return (
-      <div>
-        <img src={Logo} alt="" style={{ width: 40, height: 40 }} />
-        <div style={{ marginTop: 25, fontSize: 18 }}>
-          <div style={{ marginBottom: 15 }}>
-            <Link
-              onClick={() => this.setState({ page: "dashboard" })}
-              style={{ textDecoration: "none", color: "#111111" }}
-              to="/"
-            >
-              Dashboard
-            </Link>
-          </div>
-          <div style={{ marginBottom: 15 }}>
-            <Link
-              onClick={() => this.setState({ page: "logs" })}
-              style={{ textDecoration: "none", color: "#111111" }}
-              to="/analytics"
-            >
-              Analytics
-            </Link>
-          </div>
-          <div style={{ marginBottom: 15 }}>
-            <Link
-              onClick={() => this.setState({ page: "logs" })}
-              style={{ textDecoration: "none", color: "#111111" }}
-              to="/logs"
-            >
-              User Logs
-            </Link>
-          </div>
-          <div style={{ marginBottom: 15 }}>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://papertrailapp.com/dashboard"
-            >
-              Webserver Logs
-            </a>
-          </div>
-          <div style={{ marginBottom: 15 }}>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://s3.console.aws.amazon.com/s3/home?region=us-east-1#"
-            >
-              AWS S3
-            </a>
-          </div>{" "}
-          <div style={{ marginBottom: 15 }}>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://lightsail.aws.amazon.com/ls/webapp/home/instances?#"
-            >
-              AWS Lightsail
-            </a>
-          </div>
-          <div style={{ marginBottom: 15 }}>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/orgs/fractalcomputers/dashboard"
-            >
-              GitHub
-            </a>
-          </div>
-          <div style={{ marginBottom: 15 }}>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://drive.google.com/open?id=1kbpGbVeYd5LVJgzGN0dKro-ryizQr2kg"
-            >
-              Google Drive
-            </a>
-          </div>
-          <div style={{ marginBottom: 15 }}>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.notion.so/fractalcomputers/2fa59d87701b4ec7967377450c75008a?v=f20e6d7d6d5f4d4c9a0f886871259833"
-            >
-              Notion
-            </a>
-          </div>
-          <div style={{ marginBottom: 15 }}>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://fractalcomputers.slack.com"
-            >
-              Slack
-            </a>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        let modalClose = () => this.setState({ modalShow: false })
+        if (this.state.width > 700 && this.state.modalShow) {
+            modalClose()
+        }
+
+        return (
+            <div>
+                <img src={Logo} alt="" style={{ width: 40, height: 40 }} />
+                <div style={{ marginTop: 25, fontSize: 18 }}>
+                    <div style={{ marginBottom: 15 }}>
+                        <Link
+                            onClick={() => this.setState({ page: 'dashboard' })}
+                            style={{ textDecoration: 'none', color: '#111111' }}
+                            to="/"
+                        >
+                            Dashboard
+                        </Link>
+                    </div>
+                    <div style={{ marginBottom: 15 }}>
+                        <Link
+                            onClick={() => this.setState({ page: 'logs' })}
+                            style={{ textDecoration: 'none', color: '#111111' }}
+                            to="/analytics"
+                        >
+                            Analytics
+                        </Link>
+                    </div>
+                    <div style={{ marginBottom: 15 }}>
+                        <Link
+                            onClick={() => this.setState({ page: 'logs' })}
+                            style={{ textDecoration: 'none', color: '#111111' }}
+                            to="/logs"
+                        >
+                            User Logs
+                        </Link>
+                    </div>
+                    <div style={{ marginBottom: 15 }}>
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://papertrailapp.com/dashboard"
+                        >
+                            Webserver Logs
+                        </a>
+                    </div>
+                    <div style={{ marginBottom: 15 }}>
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://s3.console.aws.amazon.com/s3/home?region=us-east-1#"
+                        >
+                            AWS S3
+                        </a>
+                    </div>{' '}
+                    <div style={{ marginBottom: 15 }}>
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://lightsail.aws.amazon.com/ls/webapp/home/instances?#"
+                        >
+                            AWS Lightsail
+                        </a>
+                    </div>
+                    <div style={{ marginBottom: 15 }}>
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://github.com/orgs/fractalcomputers/dashboard"
+                        >
+                            GitHub
+                        </a>
+                    </div>
+                    <div style={{ marginBottom: 15 }}>
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://drive.google.com/open?id=1kbpGbVeYd5LVJgzGN0dKro-ryizQr2kg"
+                        >
+                            Google Drive
+                        </a>
+                    </div>
+                    <div style={{ marginBottom: 15 }}>
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://www.notion.so/fractalcomputers/2fa59d87701b4ec7967377450c75008a?v=f20e6d7d6d5f4d4c9a0f886871259833"
+                        >
+                            Notion
+                        </a>
+                    </div>
+                    <div style={{ marginBottom: 15 }}>
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://fractalcomputers.slack.com"
+                        >
+                            Slack
+                        </a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default LeftMenu
--- src/components/PageAdmin/components/UsageChart.js
+++ src/components/PageAdmin/components/UsageChart.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
-import { ResponsiveLine } from '@nivo/line'
 import { fetchUserReport } from '../../../actions/index.js'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
 import { connect } from 'react-redux'
 import moment from 'moment'
 import { Badge } from 'react-bootstrap'
+import {
+    LineChart,
+    Line,
+    ResponsiveContainer,
+    XAxis,
+    YAxis,
+    Tooltip,
+} from 'recharts'
 
 import Style from '../../../styles/components/analytics.module.css'
 
@@ -32,22 +39,35 @@ class UsageChart extends Component {
                 eastus = [],
                 northcentralus = [],
                 southcentralus = []
+            let component = this
             this.props.userReport.forEach((element) => {
                 total.push({
-                    x: new Date(element.timestamp * 1000),
-                    y: element.users_online,
+                    x: component.getRequiredDateFormat(
+                        new Date(element.timestamp * 1000),
+                        'h A'
+                    ),
+                    'Number of users': element.users_online,
                 })
                 eastus.push({
-                    x: new Date(element.timestamp * 1000),
-                    y: element.eastus_unavailable,
+                    x: component.getRequiredDateFormat(
+                        new Date(element.timestamp * 1000),
+                        'h A'
+                    ),
+                    'Number of users': element.eastus_unavailable,
                 })
                 northcentralus.push({
-                    x: new Date(element.timestamp * 1000),
-                    y: element.northcentralus_unavailable,
+                    x: component.getRequiredDateFormat(
+                        new Date(element.timestamp * 1000),
+                        'h A'
+                    ),
+                    'Number of users': element.northcentralus_unavailable,
                 })
                 southcentralus.push({
-                    x: new Date(element.timestamp * 1000),
-                    y: element.southcentralus_unavailable,
+                    x: component.getRequiredDateFormat(
+                        new Date(element.timestamp * 1000),
+                        'h A'
+                    ),
+                    'Number of users': element.southcentralus_unavailable,
                 })
             })
             this.setState({
@@ -60,7 +80,7 @@ class UsageChart extends Component {
     }
 
     getRequiredDateFormat = (timeStamp, format = 'MM-DD-YYYY') => {
-        return moment(timeStamp).format(format)
+        return moment(timeStamp).format(format) + ' UTC'
     }
 
     chartElement = (userType) => {
@@ -83,64 +103,41 @@ class UsageChart extends Component {
         }
         return (
             <div className={Style.usageChart}>
-                <ResponsiveLine
-                    data={[
-                        {
-                            id: 'Read',
-                            data: dataList,
-                        },
-                    ]}
-                    margin={{
-                        top: 10,
-                        right: 10,
-                        bottom: 10,
-                        left: 10,
-                    }}
-                    yScale={{
-                        type: 'linear',
-                        stacked: false,
-                    }}
-                    xScale={{
-                        type: 'time',
-                        format: 'native',
-                    }}
-                    axisBottom={null}
-                    axisLeft={null}
-                    enableGridY={false}
-                    colors={{ scheme: 'nivo' }}
-                    pointSize={10}
-                    pointColor={{ theme: 'background' }}
-                    pointBorderWidth={2}
-                    pointBorderColor={{ from: 'serieColor' }}
-                    enableArea={true}
-                    enableSlices="x"
-                    sliceTooltip={({ slice }) => {
-                        const date = slice.points[0].data.xFormatted
-                        return (
-                            <Badge variant="light">
-                                <div className={Style.chartTooltipDiv}>
-                                    <strong className="pr-2">
-                                        {`${this.getRequiredDateFormat(
-                                            date,
-                                            'h A'
-                                        )}`}
-                                    </strong>
-                                    {slice.points.map((point) => (
-                                        <div key={point.id}>
-                                            <strong
-                                                style={{
-                                                    color: point.serieColor,
-                                                }}
-                                            >
-                                                {`${point.data.yFormatted}`}
-                                            </strong>
-                                        </div>
-                                    ))}
-                                </div>
-                            </Badge>
-                        )
-                    }}
-                />
+                <ResponsiveContainer height="100%" width="100%">
+                    <LineChart data={dataList}>
+                        <Line
+                            type="monotone"
+                            dataKey="Number of users"
+                            dot={false}
+                            strokeWidth={2}
+                            stroke="#8884d8"
+                        />
+                        <XAxis
+                            dataKey="x"
+                            axisLine={false}
+                            tickLine={false}
+                            tick={{
+                                fontSize: 12,
+                                transform: 'translate(0, 10)',
+                            }}
+                        />
+                        <YAxis
+                            dataKey="Number of users"
+                            axisLine={false}
+                            tickLine={false}
+                            allowDecimals={false}
+                            tick={{
+                                fontSize: 12,
+                            }}
+                        />
+                        <Tooltip
+                            contentStyle={{
+                                border: 'none',
+                                fontSize: 14,
+                            }}
+                        />
+                    </LineChart>
+                </ResponsiveContainer>
             </div>
         )
     }
