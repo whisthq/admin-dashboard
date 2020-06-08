db11845166e8836df13987cab9edd72d963eaf8b
Jonathan <jonathanxu25@gmail.com>
2020-06-08 14:55:14 -0400

    saving progress with nivo

--- src/components/PageAdmin/components/UsageChart.js
+++ src/components/PageAdmin/components/UsageChart.js
@@ -14,7 +14,12 @@ import Style from '../../../styles/components/analytics.module.css'
 class UsageChart extends Component {
     constructor(props) {
         super(props)
-        this.state = { dataList: null }
+        this.state = {
+            total: null,
+            eastus: null,
+            northcentralus: null,
+            southcentralus: null,
+        }
     }
 
     componentDidMount() {
@@ -23,84 +28,135 @@ class UsageChart extends Component {
 
     componentDidUpdate(prevProps) {
         if (prevProps.userReport !== this.props.userReport) {
-            let data = []
+            let total = [],
+                eastus = [],
+                northcentralus = [],
+                southcentralus = []
             this.props.userReport.forEach((element) => {
-                data.push({
+                total.push({
                     x: new Date(element.timestamp * 1000),
                     y: element.users_online,
                 })
+                eastus.push({
+                    x: new Date(element.timestamp * 1000),
+                    y: element.eastus_unavailable,
+                })
+                northcentralus.push({
+                    x: new Date(element.timestamp * 1000),
+                    y: element.northcentralus_unavailable,
+                })
+                southcentralus.push({
+                    x: new Date(element.timestamp * 1000),
+                    y: element.southcentralus_unavailable,
+                })
             })
-            this.setState({ dataList: data })
+            this.setState({
+                total: total,
+                eastus: eastus,
+                northcentralus: northcentralus,
+                southcentralus: southcentralus,
+            })
         }
     }
 
     getRequiredDateFormat = (timeStamp, format = 'MM-DD-YYYY') => {
         return moment(timeStamp).format(format)
     }
+
+    chartElement = (userType) => {
+        let dataList = null
+        switch (userType) {
+            case 'total':
+                dataList = this.state.total
+                break
+            case 'eastus':
+                dataList = this.state.eastus
+                break
+            case 'northcentralus':
+                dataList = this.state.northcentralus
+                break
+            case 'southcentralus':
+                dataList = this.state.southcentralus
+                break
+            default:
+                dataList = this.state.total
+        }
+        return (
+            <div className={Style.usageChart}>
+                <ResponsiveLine
+                    data={[
+                        {
+                            id: 'Read',
+                            data: dataList,
+                        },
+                    ]}
+                    margin={{
+                        top: 10,
+                        right: 10,
+                        bottom: 10,
+                        left: 10,
+                    }}
+                    yScale={{
+                        type: 'linear',
+                        stacked: false,
+                    }}
+                    xScale={{
+                        type: 'time',
+                        format: 'native',
+                    }}
+                    axisBottom={null}
+                    axisLeft={null}
+                    enableGridY={false}
+                    colors={{ scheme: 'nivo' }}
+                    pointSize={10}
+                    pointColor={{ theme: 'background' }}
+                    pointBorderWidth={2}
+                    pointBorderColor={{ from: 'serieColor' }}
+                    enableArea={true}
+                    enableSlices="x"
+                    sliceTooltip={({ slice }) => {
+                        const date = slice.points[0].data.xFormatted
+                        return (
+                            <Badge variant="light">
+                                <div className={Style.chartTooltipDiv}>
+                                    <strong className="pr-2">
+                                        {`${this.getRequiredDateFormat(
+                                            date,
+                                            'h A'
+                                        )}`}
+                                    </strong>
+                                    {slice.points.map((point) => (
+                                        <div key={point.id}>
+                                            <strong
+                                                style={{
+                                                    color: point.serieColor,
+                                                }}
+                                            >
+                                                {`${point.data.yFormatted}`}
+                                            </strong>
+                                        </div>
+                                    ))}
+                                </div>
+                            </Badge>
+                        )
+                    }}
+                />
+            </div>
+        )
+    }
+
     render() {
-        if (this.state.dataList) {
+        if (this.state.total) {
             return (
-                <div className={Style.usageChart}>
-                    <ResponsiveLine
-                        data={[
-                            {
-                                id: 'Read',
-                                data: this.state.dataList,
-                            },
-                        ]}
-                        margin={{
-                            top: 10,
-                            right: 10,
-                            bottom: 10,
-                            left: 10,
-                        }}
-                        yScale={{
-                            type: 'linear',
-                            stacked: false,
-                        }}
-                        xScale={{
-                            type: 'time',
-                            precision: 'every hour',
-                            format: 'native',
-                        }}
-                        axisBottom={null}
-                        axisLeft={null}
-                        enableGridY={false}
-                        enableGridX={false}
-                        colors={{ scheme: 'nivo' }}
-                        pointSize={10}
-                        pointColor={{ theme: 'background' }}
-                        pointBorderWidth={2}
-                        pointBorderColor={{ from: 'serieColor' }}
-                        enableArea={true}
-                        enableSlices="x"
-                        sliceTooltip={({ slice }) => {
-                            const date = slice.points[0].data.xFormatted
-                            return (
-                                <Badge variant="light">
-                                    <div className={Style.chartTooltipDiv}>
-                                        <strong className="pr-2">
-                                            {`${this.getRequiredDateFormat(
-                                                date,
-                                                'h A'
-                                            )}`}
-                                        </strong>
-                                        {slice.points.map((point) => (
-                                            <div key={point.id}>
-                                                <strong
-                                                    style={{
-                                                        color: point.serieColor,
-                                                    }}
-                                                >
-                                                    {`${point.data.yFormatted}`}
-                                                </strong>
-                                            </div>
-                                        ))}
-                                    </div>
-                                </Badge>
-                            )
-                        }}
-                    />
+                <div>
+                    <p>Total</p>
+                    {this.chartElement('total')}
+                    <p>Eastus</p>
+                    {this.chartElement('eastus')}
+                    <p>Northcentralus</p>
+                    {this.chartElement('northcentralus')}
+                    <p>Southcentralus</p>
+                    {this.chartElement('southcentralus')}
                 </div>
             )
         } else {
