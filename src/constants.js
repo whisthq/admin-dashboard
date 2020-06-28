e098bc904ad6c8c65d6f4abedbc81f03f1d99a99
mingy98 <ming@fractalcomputers.com>
2020-06-28 16:42:59 -0400

    swapped env variables

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 const production = {
     url: {
-        PRIMARY_SERVER: 'https://cube-celery-vm.herokuapp.com',
+        PRIMARY_SERVER: 'https://cube-celery-staging.herokuapp.com',
         WEBSITE_URL: 'https://fractal-dashboard.netlify.app',
     },
     stripe: {
@@ -33,10 +33,8 @@ const development = {
     url: {
         PRIMARY_SERVER: 'http://127.0.0.1:5000',
         WEBSITE_URL: 'http://localhost:3000',
-    },
-    stripe: {
-        PUBLIC_KEY: 'pk_test_7y07LrJWC5LzNu17sybyn9ce004CLPaOXb',
+S        PUBLIC_KEY: 'pk_test_7y07LrJWC5LzNu17sybyn9ce004CLPaOXb',
     },
 }
 
-export const config = process.env.NODE_ENV === 'development' ? staging : staging
+export const config = process.env.NODE_ENV === 'development' ? staging : production
