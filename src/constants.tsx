451eeac1c424587ec6f34895305da1dc90547e5b
philippemnoel <philippe_noel@college.harvard.edu>
2020-09-01 18:49:06 -0400

    Better ts-ignore for constant file

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,14 +1,15 @@
-// const production = {
-//     url: {
-//         PRIMARY_SERVER: 'https://cube-celery-staging.herokuapp.com',
-//         WEBSITE_URL: 'https://fractal-dashboard.netlify.app',
-//         GRAPHQL: 'https://newdb-wrapper.herokuapp.com/v1/graphql',
-//     },
-//     graphQL: {
-//         SECRET: 'Fractalcomputers!',
-//     },
-//     new_server: false,
-// }
+// @ts-ignore
+const production = {
+    url: {
+        PRIMARY_SERVER: 'https://cube-celery-staging.herokuapp.com',
+        WEBSITE_URL: 'https://fractal-dashboard.netlify.app',
+        GRAPHQL: 'https://newdb-wrapper.herokuapp.com/v1/graphql',
+    },
+    graphQL: {
+        SECRET: 'Fractalcomputers!',
+    },
+    new_server: false,
+}
 
 const staging = {
     url: {
@@ -22,28 +23,30 @@ const staging = {
     new_server: true,
 }
 
-// const local = {
-//     url: {
-//         PRIMARY_SERVER: 'http://127.0.0.1:7730',
-//         WEBSITE_URL: 'http://localhost:3000',
-//         GRAPHQL: 'https://newdb-wrapper.herokuapp.com/v1/graphql',
-//     },
-//     graphQL: {
-//         SECRET: 'Fractalcomputers!',
-//     },
-//     new_server: true,
-// }
+// @ts-ignore
+const local = {
+    url: {
+        PRIMARY_SERVER: 'http://127.0.0.1:7730',
+        WEBSITE_URL: 'http://localhost:3000',
+        GRAPHQL: 'https://newdb-wrapper.herokuapp.com/v1/graphql',
+    },
+    graphQL: {
+        SECRET: 'Fractalcomputers!',
+    },
+    new_server: true,
+}
 
-// const development = {
-//     url: {
-//         PRIMARY_SERVER: 'http://127.0.0.1:5000',
-//         WEBSITE_URL: 'http://localhost:3000',
-//         GRAPHQL: 'https://newdb-wrapper.herokuapp.com/v1/graphql',
-//     },
-//     graphQL: {
-//         SECRET: 'Fractalcomputers!',
-//     },
-//     new_server: true,
-// }
+// @ts-ignore
+const development = {
+    url: {
+        PRIMARY_SERVER: 'http://127.0.0.1:5000',
+        WEBSITE_URL: 'http://localhost:3000',
+        GRAPHQL: 'https://newdb-wrapper.herokuapp.com/v1/graphql',
+    },
+    graphQL: {
+        SECRET: 'Fractalcomputers!',
+    },
+    new_server: true,
+}
 
 export const config = process.env.NODE_ENV === 'development' ? staging : staging
