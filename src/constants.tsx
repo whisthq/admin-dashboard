e47ae6604ffc0b555a964df4e1911446aee59c04
18adrianoh <adrianohernandez2000gmail.com>
2020-09-29 08:34:29 -0700

    constants needs to be updated

--- src/constants.tsx
+++ src/constants.tsx
@@ -39,18 +39,5 @@ const local = {
     new_server: true,
 }
 
-// @ts-ignore
-// eslint-disable-next-line
-const development = {
-    url: {
-        PRIMARY_SERVER: 'http://127.0.0.1:5000',
-        WEBSITE_URL: 'http://localhost:3000',
-        GRAPHQL: 'https://newdb-wrapper.herokuapp.com/v1/graphql',
-    },
-    graphQL: {
-        SECRET: 'Fractalcomputers!',
-    },
-    new_server: true,
-}
-
+// TODO undo me
 export const config = process.env.NODE_ENV === 'development' ? staging : staging
